82724fa60516fc1b08efbcb7020b5c6ec2b03245
야무치 <bb@bbb.com>
2024-02-05 17:12:56 +0900

    ㅇㅇ

---
+++ src/day01/Ex02.java
@@ -0,0 +1,64 @@
+package day01;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println("100\n200\n300");
+		System.out.println("1\t1234567\t12345678\t1");
+		System.out.println("이름\t나이");
+		System.out.println("=========");
+		System.out.println("홍길동\t20살");
+		System.out.println("김개똥\t30살");
+		System.out.println("-------------");
+		System.out.println("D:\\핀테크 Oh\\eclipse-java\\elipse");
+		System.out.println("\"\\");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	}
+}
---
+++ src/day01/Ex03.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		System.out.println(100 + 100);
+		System.out.println(1.123 + 3.456);
+		System.out.println("안녕" + "하세요");
+		System.out.println("100"+ 200);
+		System.out.println("백" + 100);
+		System.out.println("안녕100100" + 100);
+		System.out.println("안녕" + (100 + 100));
+		System.out.println("안녕" + (500 + 100));
+		
+		
+		
+	}
+}
